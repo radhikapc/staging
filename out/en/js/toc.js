a98f0a7243e2b72425f342f1865307a1c15fdedc
Radhika Puthiyetath <radhika.dwaraka@gmail.com>
2019-06-12 11:51:59 -0700

    testing continuous integration with netlify

---
+++ out/en/js/toc.js
@@ -0,0 +1,132 @@
+
+			
+			
+			var toc = [{
+"url": "26496-26500-sysdig-monitor-documentation-hub.html",
+"text": "Sysdig Monitor Documentation Hub"
+,
+"children": [
+{
+"url": "26496-26503-getting-started.html",
+"text": "Getting Started"
+}
+,
+{
+"url": "26496-26504-explore.html",
+"text": "Explore"
+,
+"children": [
+{
+"url": "26496-26505-the-explore-table.html",
+"text": "The Explore Table"
+}
+,
+{
+"url": "26496-26506-time-windows.html",
+"text": "Time Windows"
+}
+]
+}
+,
+{
+"url": "26496-26507-metrics.html",
+"text": "Metrics"
+,
+"children": [
+{
+"url": "26496-26508-data-aggregation.html",
+"text": "Data Aggregation"
+}
+,
+{
+"url": "26496-26509-data-retention.html",
+"text": "Data Retention"
+}
+,
+{
+"url": "26496-26510-manage-metric-scale.html",
+"text": "Manage Metric Scale"
+}
+]
+}
+]
+}
+,
+{
+"url": "26496-26511-sysdig-secure-documentation.html",
+"text": "Sysdig Secure Documentation"
+,
+"children": [
+{
+"url": "26496-26512-getting-started.html",
+"text": "Getting Started"
+}
+,
+{
+"url": "26496-26513-policy-events.html",
+"text": "Policy Events"
+,
+"children": [
+{
+"url": "26496-26514-configure-groupings-in-sysdig-secure.html",
+"text": "Configure Groupings in Sysdig Secure"
+}
+,
+{
+"url": "26496-26515-identify-false-positives.html",
+"text": "Identify False Positives"
+}
+,
+{
+"url": "26496-26516-event-forwarding.html",
+"text": "Event Forwarding"
+}
+,
+{
+"url": "26496-26517-kubernetes-audit-logging.html",
+"text": "Kubernetes Audit Logging"
+}
+]
+}
+]
+}
+,
+{
+"url": "26496-26518-sysdig-developer-documentation.html",
+"text": "Sysdig Developer Documentation"
+}
+,
+{
+"url": "26496-26519-sysdig-integrations.html",
+"text": "Sysdig Integrations"
+,
+"children": [
+{
+"url": "26496-26520-integrate-jmx-metrics-from-java-virtual-machines.html",
+"text": "Integrate JMX Metrics from Java Virtual Machines"
+}
+,
+{
+"url": "26496-26521-integrate-statsd-metrics.html",
+"text": "Integrate StatsD Metrics"
+}
+,
+{
+"url": "26496-26522-integrate-node-js-application-metrics.html",
+"text": "Integrate Node.js Application Metrics"
+}
+,
+{
+"url": "26496-26523-integrate-prometheus-metrics-into-sysdig-monitor-ui.html",
+"text": "Integrate Prometheus Metrics into Sysdig Monitor UI"
+}
+,
+{
+"url": "26496-26524-monitor-log-files.html",
+"text": "Monitor Log Files"
+}
+]
+}
+];
+			
+		
