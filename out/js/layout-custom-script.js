8c092e7974e517d75d4ca7cce516dcce4f05dbf7
Radhika Puthiyetath <radhika.dwaraka@gmail.com>
2019-07-30 15:45:10 -0700

    new styling

--- out/js/layout-custom-script.js
+++ out/js/layout-custom-script.js
@@ -1,4 +1,4 @@
 $( document ).ready(function() {
   $('.portal-header h1').text('Welcome to the Sysdig Documentation Hub!');
-  $( "<p class='intro-para'> Continue your Sysdig journey with Sysdig Documentation. </p>" ).insertAfter('.portal-search');
+  //$( "<p class='intro-para'> Continue your Sysdig journey with Sysdig Documentation. </p>" ).insertAfter('.portal-search');
  });
