16d65c2b9d40404c7982d0c43ce1c8a6a6000d04
Radhika Puthiyetath <radhika.dwaraka@gmail.com>
2019-09-16 11:02:32 -0700

    fixed the layout

--- out/js/layout-custom-script.js
+++ out/js/layout-custom-script.js
@@ -2,6 +2,9 @@
   var icon1 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/03/nav-icon-secure.svg";
   var icon2 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/nav-icon-monitor-2.svg";
   var icon3 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/03/nav-icon-platform.svg";
+  var icon4 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/icon-opensource.svg";
+  var icon5 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/09/Group.png";
+  var icon6 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/Documentation.png"
 
   $(".cat-panel-1").closest("a").prepend("<img src='" + icon1 + "'></img>");
   $(".cat-panel-2").closest("a").prepend("<img src='" + icon2 + "'></img>");
