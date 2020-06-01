8256ac9dd437646f7bd4cb404882f446fcec1290
Radhika Puthiyetath <radhika.dwaraka@gmail.com>
2020-06-01 13:59:32 -0700

    agent profiling and new icons

--- out/js/layout-custom-script.js
+++ out/js/layout-custom-script.js
@@ -1,7 +1,7 @@
  $(document).ready(function () {
-  var icon1 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/03/nav-icon-secure.svg";
-  var icon2 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/nav-icon-monitor-2.svg";
-  var icon3 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/03/nav-icon-platform.svg";
+  var icon1 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/docs-nav-icon-monitor.svg";
+  var icon2 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/docs-nav-icon-secure.svg";
+  var icon3 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/docs-nav-icon-platform.svg";
   var icon4 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/icon-opensource.svg";
   var icon5 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/09/Group.png";
   var icon6 = "https://478h5m1yrfsa3bbe262u7muv-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/Documentation.png";
@@ -13,7 +13,7 @@
   $(".cat-panel-5").closest("a").prepend("<img src='" + icon5 + "'></img>");
   $(".cat-panel-6").closest("a").prepend("<img src='" + icon6 + "'></img>");
   $(".publication-icon").remove();
-  $('.portal-header h1').text('Welcome to the Sysdig Staging Documentation Site!');
-  $( "<p class='intro-para'> Explore the new sections we have added recently. We encourage you to report issues by filing JIRA Tickets.</p>" ).insertAfter('.portal-search')
+  $('.portal-header h1').text('Welcome to the Sysdig Documentation Hub!');
+  $( "<p class='intro-para'>Explore the new sections we have added recently. We encourage you to report issues by filing JIRA Tickets.</a>.</p>" ).insertAfter('.portal-search')
   //$( "<p class='intro-para'> This is our brand-new Doc site. You may encounter some broken links and missing images. We are actively working on it. <br />In the meantime, explore the site and let us know what you think. Send your feedback to <a>doc-feedback@sysdig.com</a>.</p>" ).insertAfter('.portal-search')
  });
